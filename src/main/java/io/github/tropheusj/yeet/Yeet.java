950298c0ae99d35c48bef6a7ef3b639d054aa03e
TropheusJ <jverrellijr@icloud.com>
2023-09-19 16:45:12 -0400

    tweak timings and powers

--- src/main/java/io/github/tropheusj/yeet/Yeet.java
+++ src/main/java/io/github/tropheusj/yeet/Yeet.java
@@ -2,7 +2,6 @@ package io.github.tropheusj.yeet;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
-import net.minecraft.world.item.BowItem;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
@@ -18,11 +17,13 @@ public class Yeet implements ModInitializer {
 
 	public static final int TICKS_FOR_MAX_WIND_UP = (int) (1.5 * 20);
 	public static final int TICKS_FOR_SUPERCHARGE_1 = 4 * 20;
-	public static final int TICKS_FOR_SUPERCHARGE_2 = 6 * 20;
+	public static final int TICKS_FOR_SUPERCHARGE_2 = 7 * 20;
 
-	// bow power maxes out at 20 ticks
-	public static final float SUPERCHARGE_1_POWER = BowItem.getPowerForTime(20) * 1.5f;
-	public static final float SUPERCHARGE_2_POWER = BowItem.getPowerForTime(20) * 2;
+	// power is a multiplier on the thrown item velocity
+	// bow maxes out at 1
+	public static final float MAX_WIND_UP_POWER = 1;
+	public static final float SUPERCHARGE_1_POWER = MAX_WIND_UP_POWER * 1.5f;
+	public static final float SUPERCHARGE_2_POWER = MAX_WIND_UP_POWER * 2;
 
 	@Override
 	public void onInitialize(ModContainer mod) {
@@ -51,7 +52,7 @@ public class Yeet implements ModInitializer {
 		} else if (chargeTicks >= TICKS_FOR_SUPERCHARGE_1) {
 			return SUPERCHARGE_1_POWER;
 		} else {
-			return BowItem.getPowerForTime(chargeTicks);
+			return getWindUp(chargeTicks, 0, 0, MAX_WIND_UP_POWER);
 		}
 	}
 }
